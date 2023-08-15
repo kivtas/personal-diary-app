57f0cbe0c8696b77b492b8f47159f9e4b4c5c07a
skdev <skdev@ucsc.edu>
2023-08-15 13:39:13 -0700

    some basics

---
+++ frontend/src/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <div className="home">
+            <h2>Placeholder for diary entries</h2>
+        </div>  
+
+    );
+}
+ 
+export default Home;
---
+++ frontend/src/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return (  
+        <nav className="navbar">
+            <h1>My Diary</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New Entry</a>
+                <a href="/profile">Profile Insights</a>
+                <a href="/logout">Log Out</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
